5c60195bc0fa588b02bc244ad2c25c27a10c4824
skyline10101 <418523223@qq.com>
2017-08-24 00:22:28 +0800

    first 2 lessions,build project structure,config webpack,hot loader,need to learn 3-1

---
+++ app/index.js
@@ -0,0 +1,27 @@
+//var react = require('react');
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import Hello from './components/hello';
+
+
+// console.log(React.version);
+
+render(
+	<AppContainer>
+		<Hello></Hello>
+	</AppContainer>,
+	document.getElementById('root')
+);
+
+if (module.hot) {
+    module.hot.accept('./components/hello', () => {
+        const NewHello = require('./components/hello').default;
+        render(
+            <AppContainer>
+                <NewHello />
+            </AppContainer>,
+            document.getElementById('root')
+        );
+    });
+}
